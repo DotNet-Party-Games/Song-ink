99ac5dee82fb2ef40700a5f9a9a26bee93e5ce24
Jacob Maniscalco <jmaniscalco9@icloud.com>
2021-08-19 15:29:30 -0400

    Corrected method names

--- Back-End/SongsinkWebApi/Controllers/MainController.cs
+++ Back-End/SongsinkWebApi/Controllers/MainController.cs
@@ -102,25 +102,25 @@ namespace SongsinkWebApi.Controllers
         [HttpGet("getPlayerWords/{p_id}")]
         public async Task<IActionResult> getPlayerWords(int p_id)
         {
-            return Ok(await _BL.getPlayerWords(p_id));
+            return Ok(await _BL.GetPlayerWords(p_id));
         }
 
         [HttpGet("getCustomCategories/{p_id}")]
         public async Task<IActionResult> getCustomCategories(int p_id)
         {
-            return Ok(await _BL.getCustomCategories(p_id));
+            return Ok(await _BL.GetCustomCategories(p_id));
         }
 
         [HttpPost("addCustomCategory")]
         public async Task<IActionResult> addCustomCategory(CustomCategory p_category)
         {
-            return Created("api/Main/addCustomCategory", await _BL.addCustomCategory(p_category));
+            return Created("api/Main/addCustomCategory", await _BL.AddCustomCategory(p_category));
         }
 
         [HttpPost("addPlayerWord")]
         public async Task<IActionResult> addPlayerWord(CustomWord p_word)
         {
-            return Created("api/Main/addPlayerWord", await _BL.addPlayerWord(p_word));
+            return Created("api/Main/addPlayerWord", await _BL.AddPlayerWord(p_word));
         }
     }
 }
