04be3bd5b604310645aee561a92d01fcca634dc4
Jacob Maniscalco <jmaniscalco9@icloud.com>
2021-08-19 14:19:24 -0400

    Added DL and BL logic retrieve custom words and categories from the db

--- Back-End/SongsinkBL/IBL.cs
+++ Back-End/SongsinkBL/IBL.cs
@@ -109,5 +109,32 @@ namespace SongsinkBL
         /// <param name="p_player"></param>
         /// <returns></returns>
         Task<Player> CreateNewPlayer(Player p_player);
+
+         /// <summary>
+        /// Gets all custom words created by a user
+        /// </summary>
+        /// <returns>List of custom words</returns>
+        Task<List<CustomWord>> GetPlayerWords(int p_userID);
+
+        /// <summary>
+        /// Adds a custom word to the corresponding user
+        /// </summary>
+        /// <param name="p_word"> The word to be added</param>
+        /// <returns> The custom word that was added</returns>
+        Task<CustomWord> AddPlayerWord(CustomWord p_word);
+        
+        /// <summary>
+        /// Adds a custom category for the user
+        /// </summary>
+        /// <param name="p_category"> contains the category name and player id</param>
+        /// <returns> the custom category that was created </returns>
+        Task<CustomCategory> AddCustomCategory(CustomCategory p_category);
+
+        /// <summary>
+        /// Gets the custom categories of the specified user
+        /// </summary>
+        /// <param name="p_playerID">The ID of the user </param>
+        /// <returns>Returns a list of custom categories</returns>
+        Task<List<CustomCategory>> GetCustomCategories(int p_playerID);
     }
 }
