4d32edac4cc082eb5e2b29cfe7354fb15aea273a
Jacob Maniscalco <jmaniscalco9@icloud.com>
2021-08-25 12:50:07 -0400

    Removed bugs and code smells from sonarcloud analysis

--- Back-End/SongsinkModel/CustomWord.cs
+++ Back-End/SongsinkModel/CustomWord.cs
@@ -9,32 +9,13 @@ namespace SongsinkModel
 {
 	public class CustomWord
 	{
-		private int _id;
-		private int _playerId;
-		private string _customWordName;
-		private int _customCategoryId;
-
 		public CustomWord()
-		{
-		}
+		{ }
 
-        public int Id { get => _id; set => _id = value; }
-		public int PlayerId { get => _playerId; set => _playerId = value; }
-		public string CustomWordName {
-			get
-			{
-				return CustomWordName;
-			}
-			set
-			{
-				if (!Regex.IsMatch(value, @"^[A-Za-z]+$"))
-				{
-					throw new Exception("Invalid Input [A-Za-z]");
-				}
-				CustomWordName = value;
-			}
-		}
+        public int Id { get; set;}
+		public int PlayerId { get; set; }
+		public string CustomWordName { get; set; }
 
-        public int CustomCategoryId { get => _customCategoryId; set => _customCategoryId = value; }
+        public int CustomCategoryId { get; set; }
     }
 }
